1001b8108494026849248fbc3c81e37be2605d8f
Jack <yebin@star-net.cn>
2020-03-14 09:12:50 +0800

    添加前端工程

---
+++ cloud_sign_frond_vue/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <kmDialog title="这是一个测试dialog" content="测试展示内容" buttons />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import kmDialog from "../components/dialog/km-dialog.vue";
+
+@Component({
+  components: {
+    kmDialog
+  }
+})
+export default class Home extends Vue {}
+</script>
