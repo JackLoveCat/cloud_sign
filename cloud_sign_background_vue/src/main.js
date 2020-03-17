79e34a94ae36cc9a87174f65f4ae288473d12134
chouchouxia <1804235271@qq.com>
2020-03-17 22:13:31 +0800

    登录页面修改

--- cloud_sign_background_vue/src/main.js
+++ cloud_sign_background_vue/src/main.js
@@ -26,7 +26,6 @@ if (process.env.NODE_ENV !== 'production') {
 Vue.prototype.$http = httpRequest // ajax请求方法
 Vue.prototype.isAuth = isAuth     // 权限方法
 
-
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
 
