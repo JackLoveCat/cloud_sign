5cc7d28379470fe51dca12b86fee1d38a0973bc3
190325003-范学南 <fanxuenan@126.com>
2020-03-17 21:51:59 +0800

    Demo

--- cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/demo/controller/DemoController.java
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/demo/controller/DemoController.java
@@ -12,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import cn.edu.fzu.cloudsign.demo.domain.Demo;
 import cn.edu.fzu.cloudsign.framework.web.domain.AjaxResult;
 
+/**
+ * Demo
+ * @author fanxu
+ *
+ */
 @RestController
-@RequestMapping("/api/demo")
+@RequestMapping("/demo")
 public class DemoController {
 
 	@GetMapping("/getdemo")
