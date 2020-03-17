792519fd59e152eec3a42d04c24d5b6da82cbc2d
190325003-范学南 <fanxuenan@126.com>
2020-03-17 22:03:36 +0800

    测试冲突

--- cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/demo/controller/DemoController.java
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/demo/controller/DemoController.java
@@ -4,12 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.edu.fzu.cloudsign.demo.domain.Demo;
 import cn.edu.fzu.cloudsign.framework.web.domain.AjaxResult;
 
 /**
