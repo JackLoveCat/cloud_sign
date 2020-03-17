afd4912f14f418edb5d4d693dd4ca57b824abc2b
190325003-范学南 <fanxuenan@126.com>
2020-03-17 15:18:05 +0800

    班课功能模块和系统基础功能

---
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/project/system/controller/SysMenuController.java
@@ -0,0 +1,116 @@
+package cn.edu.fzu.cloudsign.project.system.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.edu.fzu.cloudsign.common.constant.UserConstants;
+import cn.edu.fzu.cloudsign.common.utils.SecurityUtils;
+import cn.edu.fzu.cloudsign.common.utils.ServletUtils;
+import cn.edu.fzu.cloudsign.framework.aspectj.lang.annotation.Log;
+import cn.edu.fzu.cloudsign.framework.aspectj.lang.enums.BusinessType;
+import cn.edu.fzu.cloudsign.framework.security.LoginUser;
+import cn.edu.fzu.cloudsign.framework.security.service.TokenService;
+import cn.edu.fzu.cloudsign.framework.web.controller.BaseController;
+import cn.edu.fzu.cloudsign.framework.web.domain.AjaxResult;
+import cn.edu.fzu.cloudsign.project.system.domain.SysMenu;
+import cn.edu.fzu.cloudsign.project.system.service.ISysMenuService;
+
+/**
+ * 菜单信息
+ * 
+ */
+@RestController
+@RequestMapping("/system/menu")
+public class SysMenuController extends BaseController {
+	@Autowired
+	private ISysMenuService menuService;
+
+	@Autowired
+	private TokenService tokenService;
+
+	/**
+	 * 获取菜单列表
+	 */
+	@GetMapping("/list")
+	public AjaxResult list(SysMenu menu) {
+		LoginUser loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+		Long userId = loginUser.getUser().getUserId();
+		List<SysMenu> menus = menuService.selectMenuList(menu, userId);
+		return AjaxResult.success(menus);
+	}
+
+	/**
+	 * 获取菜单下拉树列表
+	 */
+	@GetMapping("/treeselect")
+	public AjaxResult treeselect(SysMenu menu) {
+		LoginUser loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+		Long userId = loginUser.getUser().getUserId();
+		List<SysMenu> menus = menuService.selectMenuList(menu, userId);
+		return AjaxResult.success(menuService.buildMenuTreeSelect(menus));
+	}
+
+	/**
+	 * 根据菜单编号获取详细信息
+	 */
+	@PreAuthorize("@ss.hasPermi('system:menu:query')")
+	@GetMapping(value = "/{menuId}")
+	public AjaxResult getInfo(@PathVariable Long menuId) {
+		return AjaxResult.success(menuService.selectMenuById(menuId));
+	}
+
+	/**
+	 * 新增菜单
+	 */
+	@PreAuthorize("@ss.hasPermi('system:menu:add')")
+	@Log(title = "菜单管理", businessType = BusinessType.INSERT)
+	@PostMapping
+	public AjaxResult add(@Validated @RequestBody SysMenu menu) {
+		if (UserConstants.NOT_UNIQUE.equals(menuService.checkMenuNameUnique(menu))) {
+			return AjaxResult.error("新增菜单'" + menu.getMenuName() + "'失败，菜单名称已存在");
+		}
+		menu.setCreateBy(SecurityUtils.getUsername());
+		return toAjax(menuService.insertMenu(menu));
+	}
+
+	/**
+	 * 修改菜单
+	 */
+	@PreAuthorize("@ss.hasPermi('system:menu:edit')")
+	@Log(title = "菜单管理", businessType = BusinessType.UPDATE)
+	@PutMapping
+	public AjaxResult edit(@Validated @RequestBody SysMenu menu) {
+		if (UserConstants.NOT_UNIQUE.equals(menuService.checkMenuNameUnique(menu))) {
+			return AjaxResult.error("修改菜单'" + menu.getMenuName() + "'失败，菜单名称已存在");
+		}
+		menu.setUpdateBy(SecurityUtils.getUsername());
+		return toAjax(menuService.updateMenu(menu));
+	}
+
+	/**
+	 * 删除菜单
+	 */
+	@PreAuthorize("@ss.hasPermi('system:menu:remove')")
+	@Log(title = "菜单管理", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{menuId}")
+	public AjaxResult remove(@PathVariable("menuId") Long menuId) {
+		if (menuService.hasChildByMenuId(menuId)) {
+			return AjaxResult.error("存在子菜单,不允许删除");
+		}
+		if (menuService.checkMenuExistRole(menuId)) {
+			return AjaxResult.error("菜单已分配,不允许删除");
+		}
+		return toAjax(menuService.deleteMenuById(menuId));
+	}
+}
---
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/project/system/domain/SysMenu.java
@@ -0,0 +1,143 @@
+package cn.edu.fzu.cloudsign.project.system.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import cn.edu.fzu.cloudsign.framework.web.domain.BaseEntity;
+
+/**
+ * 菜单权限表 sys_menu
+ * 
+ */
+public class SysMenu extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	/** 菜单ID */
+	private Long menuId;
+
+	/** 菜单名称 */
+	private String menuName;
+
+	/** 父菜单名称 */
+	private String parentName;
+
+	/** 父菜单ID */
+	private Long parentId;
+
+	/** 菜单图标 */
+	private String icon;
+
+	/** 菜单链接 */
+	private String link;
+
+	/** 显示顺序 */
+	private String orderNum;
+
+	/** 是否菜单 */
+	private String isMenu;
+
+	/** 是否页面 */
+	private String isPage;
+
+	/** 子菜单 */
+	private List<SysMenu> children = new ArrayList<SysMenu>();
+
+	public Long getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(Long menuId) {
+		this.menuId = menuId;
+	}
+
+	@NotBlank(message = "菜单名称不能为空")
+	@Size(min = 0, max = 50, message = "菜单名称长度不能超过50个字符")
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public Long getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+
+	@NotBlank(message = "显示顺序不能为空")
+	public String getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public List<SysMenu> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<SysMenu> children) {
+		this.children = children;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getIsMenu() {
+		return isMenu;
+	}
+
+	public void setIsMenu(String isMenu) {
+		this.isMenu = isMenu;
+	}
+
+	public String getIsPage() {
+		return isPage;
+	}
+
+	public void setIsPage(String isPage) {
+		this.isPage = isPage;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).append("menuId", getMenuId())
+				.append("menuName", getMenuName()).append("parentId", getParentId()).append("orderNum", getOrderNum())
+				.append("icon", getIcon()).append("createBy", getCreateBy()).append("createTime", getCreateTime())
+				.append("updateBy", getUpdateBy()).append("updateTime", getUpdateTime()).append("remark", getRemark())
+				.toString();
+	}
+}
