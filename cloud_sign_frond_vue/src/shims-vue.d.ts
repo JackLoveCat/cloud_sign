9c3788dae2086bb689fed4669af37e421d4834f7
190325014_叶斌 <yebin@star-net.cn>
2020-03-20 08:14:13 +0800

    添加前段框架

--- cloud_sign_frond_vue/src/shims-vue.d.ts
+++ cloud_sign_frond_vue/src/shims-vue.d.ts
@@ -1,5 +1,17 @@
-declare module '*.vue' {
-  import Vue from 'vue';
+import Vue from "vue";
+import VueRouter, { Route } from "vue-router";
 
+declare module "*.vue" {
   export default Vue;
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $router: VueRouter; // 这表示this下有这个东西
+    $route: Route;
+    $https: any;
+    $urls: any;
+    $Message: any;
+    $Modal: any;
+  }
 }
