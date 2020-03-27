64ae02ca2ace44679feca6585e7e97a972aee711
anjorys@163.com <anjory@DESKTOP-IAGCBG1>
2020-03-27 22:19:48 +0800

    A:增加云班课数据字典类型、数据字典信息服务代码

---
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/project/sysdicttype/service/IDictTypeService.java
@@ -0,0 +1,61 @@
+package cn.edu.fzu.cloudsign.project.sysdicttype.service;
+
+import java.util.List;
+import cn.edu.fzu.cloudsign.project.sysdicttype.domain.DictType;
+
+/**
+ * 字典类型Service接口
+ * 
+ * @author linkai
+ * @date 2020-03-27
+ */
+public interface IDictTypeService 
+{
+    /**
+     * 查询字典类型
+     * 
+     * @param dictTypeId 字典类型ID
+     * @return 字典类型
+     */
+    public DictType selectDictTypeById(Long dictTypeId);
+
+    /**
+     * 查询字典类型列表
+     * 
+     * @param dictType 字典类型
+     * @return 字典类型集合
+     */
+    public List<DictType> selectDictTypeList(DictType dictType);
+
+    /**
+     * 新增字典类型
+     * 
+     * @param dictType 字典类型
+     * @return 结果
+     */
+    public int insertDictType(DictType dictType);
+
+    /**
+     * 修改字典类型
+     * 
+     * @param dictType 字典类型
+     * @return 结果
+     */
+    public int updateDictType(DictType dictType);
+
+    /**
+     * 批量删除字典类型
+     * 
+     * @param dictTypeIds 需要删除的字典类型ID
+     * @return 结果
+     */
+    public int deleteDictTypeByIds(Long[] dictTypeIds);
+
+    /**
+     * 删除字典类型信息
+     * 
+     * @param dictTypeId 字典类型ID
+     * @return 结果
+     */
+    public int deleteDictTypeById(Long dictTypeId);
+}
