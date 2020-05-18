422c9bd7aa435f9d8a8e8bcc420de61afba931a5
190325003-范学南 <fanxuenan@126.com>
2020-05-18 22:28:01 +0800

    selectRoleById增加返回MenuIds

--- cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/project/system/mapper/SysRoleMenuMapper.java
+++ cloud_sign_backend_java/src/main/java/cn/edu/fzu/cloudsign/project/system/mapper/SysRoleMenuMapper.java
@@ -8,29 +8,36 @@ import cn.edu.fzu.cloudsign.project.system.domain.SysRoleMenu;
  * 角色与菜单关联表 数据层
  * 
  */
-public interface SysRoleMenuMapper
-{
-    /**
-     * 查询菜单使用数量
-     * 
-     * @param menuId 菜单ID
-     * @return 结果
-     */
-    public int checkMenuExistRole(Long menuId);
+public interface SysRoleMenuMapper {
+	/**
+	 * 查询菜单使用数量
+	 * 
+	 * @param menuId 菜单ID
+	 * @return 结果
+	 */
+	public int checkMenuExistRole(Long menuId);
 
-    /**
-     * 通过角色ID删除角色和菜单关联
-     * 
-     * @param roleId 角色ID
-     * @return 结果
-     */
-    public int deleteRoleMenuByRoleId(Long roleId);
+	/**
+	 * 通过角色ID删除角色和菜单关联
+	 * 
+	 * @param roleId 角色ID
+	 * @return 结果
+	 */
+	public int deleteRoleMenuByRoleId(Long roleId);
 
-    /**
-     * 批量新增角色菜单信息
-     * 
-     * @param roleMenuList 角色菜单列表
-     * @return 结果
-     */
-    public int batchRoleMenu(List<SysRoleMenu> roleMenuList);
+	/**
+	 * 批量新增角色菜单信息
+	 * 
+	 * @param roleMenuList 角色菜单列表
+	 * @return 结果
+	 */
+	public int batchRoleMenu(List<SysRoleMenu> roleMenuList);
+
+	/**
+	 * 按角色Id查询菜单Ids
+	 * 
+	 * @param roleId
+	 * @return
+	 */
+	public Long[] selectMenuIdsByRoleId(Long roleId);
 }
